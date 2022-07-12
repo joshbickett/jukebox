176831f54ec79bb8f3dbea7db104817897ff323f
Josh Bickett <josh@bickett.net>
2022-07-11 23:31:09 -0700

    update test to correct data type

--- jukebox/test.py
+++ jukebox/test.py
@@ -5,6 +5,6 @@ def test(talk_more=False):
     if talk_more: 
       print("Hello, how are you?")
 
-run(model='1b_lyrics', name='sample_1b', levels=3, sample_length_in_seconds=20, total_sample_length_in_seconds=180, sr=44100, n_samples=3, hop_fraction=[0.5,0.5,0.125])
+run(model='1b_lyrics', name='sample_1b', levels=3, sample_length_in_seconds=20, total_sample_length_in_seconds=180, sr=44100, n_samples=3, hop_fraction=(0.5,0.5,0.125))
 # test(talk_more=True);
 
