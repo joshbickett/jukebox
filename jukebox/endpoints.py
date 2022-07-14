34bd65dbb8fcfc02af56fbd03183cff9a29bdc0e
Josh Bickett <josh@bickett.net>
2022-07-13 22:10:21 -0700

    Add make endpoint

--- jukebox/endpoints.py
+++ jukebox/endpoints.py
@@ -34,5 +34,6 @@ def save_email():
 
 @app.route("/make", methods=['POST', 'GET'])
 def make():
-    run(model='1b_lyrics', name='sample_1b', levels=3, sample_length_in_seconds=20, total_sample_length_in_seconds=180, sr=44100, n_samples=3, hop_fraction=(0.5,0.5,0.125))
+    info = { "artist": "The Beatles", "genre": "Rock", "lyrics": "Once upon a time I found a dime, it was quite find for a kid that's nine"}
+    run(model='1b_lyrics', name='sample_1b', levels=3, sample_length_in_seconds=20, total_sample_length_in_seconds=180, sr=44100, n_samples=3, hop_fraction=(0.5,0.5,0.125), song_info=info)
 
